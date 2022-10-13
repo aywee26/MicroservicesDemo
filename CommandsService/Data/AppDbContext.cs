575c30820773a377c6e154648435aaa34d6acb49
aywee26 <aywee26@gmail.com>
2022-10-13 22:27:21 +0300

    Implement Data Layer - Models, DbContext, Repository.

---
+++ CommandsService/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using CommandsService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandsService.Data;
+
+public class AppDbContext : DbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+	{
+	}
+
+	public DbSet<Platform> Platforms => Set<Platform>();
+	public DbSet<Command> Commands => Set<Command>();
+
+	protected override void OnModelCreating(ModelBuilder modelBuidler)
+	{
+		modelBuidler
+			.Entity<Platform>()
+			.HasMany(p => p.Commands)
+			.WithOne(p => p.Platform!)
+			.HasForeignKey(p => p.PlatformId);
+
+		modelBuidler
+			.Entity<Command>()
+			.HasOne(p => p.Platform)
+			.WithMany(p => p.Commands)
+			.HasForeignKey(p => p.PlatformId);
+	}
+}
