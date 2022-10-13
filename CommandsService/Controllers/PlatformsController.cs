467312e1ef132de31f4563fa84f011ad7193d4bc
aywee26 <aywee26@gmail.com>
2022-10-13 23:57:23 +0300

    Implement controllers for CommandsService.

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers;
@@ -7,9 +9,21 @@ namespace CommandsService.Controllers;
 [ApiController]
 public class PlatformsController : ControllerBase
 {
-	public PlatformsController()
-	{
+	private readonly ICommandRepo _repository;
+	private readonly IMapper _mapper;
 
+	public PlatformsController(ICommandRepo repository, IMapper mapper)
+	{
+		_repository = repository;
+		_mapper = mapper;
+	}
+
+	[HttpGet]
+	public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+	{
+		Console.WriteLine("Getting platforms from CommandsService...");
+		var platformItmes = _repository.GetAllPlatforms();
+		return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItmes));
 	}
 
 	[HttpPost]
