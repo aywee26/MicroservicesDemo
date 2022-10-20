dbada3d6e76f2b2d891d490bc82ce243a507f2ab
aywee26 <aywee26@gmail.com>
2022-10-20 19:41:28 +0300

    Add a gRPC server to PlatformService.

--- PlatformService/Profiles/PlatformsProfile.cs
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -10,7 +10,20 @@ public class PlatformsProfile : Profile
 	{
 		// source -> target
 		CreateMap<Platform, PlatformReadDto>();
+
 		CreateMap<PlatformCreateDto, Platform>();
+
 		CreateMap<PlatformReadDto, PlatformPublishDto>();
+
+		CreateMap<Platform, GrpcPlatformModel>()
+			.ForMember(
+				dest => dest.PlatformId,
+				opt => opt.MapFrom(src => src.Id))
+			.ForMember(
+				dest => dest.Name,
+                opt => opt.MapFrom(src => src.Name))
+            .ForMember(
+                dest => dest.Publisher,
+                opt => opt.MapFrom(src => src.Publisher));
 	}
 }
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.AsyncDataServices;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Grpc;
 using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,30 +23,38 @@ else
 }
 
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 builder.Services.AddSingleton<IMessageBusClient, MessageBusClient>();
+builder.Services.AddGrpc();
+
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 Console.WriteLine($"Command Service Endpoint: {builder.Configuration["CommandService"]}");
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-}
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
 //app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapGrpcService<GrpcPlatformService>();
+    endpoints.MapGet("/protos/platforms.proto", async context =>
+    {
+        await context.Response.WriteAsync(File.ReadAllText("Protos/platforms.proto"));
+    });
+});
 
 PrepDb.PrepPopulation(app, app.Environment.IsProduction());
 
